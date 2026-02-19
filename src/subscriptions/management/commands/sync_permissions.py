ccf6486b21f18e9a0bfaf6681853e9f455552ecd
imismim <arsenkorytskyi@gmail.com>
2026-02-19 18:16:11 +0200

    chore: refactor sync_permissions function and improve output styling

--- src/subscriptions/management/commands/sync_permissions.py
+++ src/subscriptions/management/commands/sync_permissions.py
@@ -1,17 +1,11 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission 
-from subscriptions.models import Subscription
 
+from subscriptions.models import Subscription
+from subscriptions.utils import sync_permissions
 class Command(BaseCommand):
     help = 'Sy nc subscriptions with groups and permissions'
 
     def handle(self, *args, **kwargs):
-        qs = Subscription.objects.filter(active=True)
-        for sub in qs:
-            perms = sub.permissions.all()
-            for group in sub.groups.all():
-                group.permissions.set(perms) 
-        
-        self.stdout.write(self.style.SUCCESS('Successfully synced subscriptions'))
-                
+        sync_permissions(self=self)
             
--- src/subscriptions/utils.py
+++ src/subscriptions/utils.py
@@ -1,10 +1,11 @@
 from helpers.billing import get_customer_list_subscriptions, cancel_subscription
 from customers.models import Customer
-from subscriptions.models import UserSubscription
+from subscriptions.models import UserSubscription, Subscription
 
 
 def clear_dangling_subs(self=None):
     out = self.stdout.write if self else print
+    style_success = self.style.SUCCESS if self else lambda x: x
     
     qs = Customer.objects.filter(stripe_id__isnull=False)
     for customer_obj in qs:
@@ -21,8 +22,19 @@ def clear_dangling_subs(self=None):
                 if existing_user_subs_qs.exists():
                     continue
                 cancel_subscription(stripe_id=sub_id, reason="Dangling old subscription", cancel_at_period_end=False)
-                out(f"--Deactivating old sub {sub_id}")
+                out(style_success(f"--Deactivating old sub {sub_id}"))
         
-    out("Syncing complete.")
+    out(style_success("Syncing complete."))
                   
             
+            
+def sync_permissions(self=None):
+    out = self.stdout.write if self else print
+    style_success = self.style.SUCCESS if self else lambda x: x
+    qs = Subscription.objects.filter(active=True)
+    for sub in qs:
+        perms = sub.permissions.all()
+        for group in sub.groups.all():
+            group.permissions.set(perms) 
+        
+    out(style_success('Successfully synced subscriptions'))
