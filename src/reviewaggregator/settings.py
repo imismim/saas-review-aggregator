ef143819656e0710c5e2c0417d061ba4032c76de
imismim <arsenkorytskyi@gmail.com>
2026-02-20 17:30:33 +0200

    fix: add missing newline after 'django.contrib.sites' in INSTALLED_APPS

--- src/reviewaggregator/settings.py
+++ src/reviewaggregator/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django.contrib.sites',
+    
     # Third-party apps
     'rest_framework',
     'allauth',
