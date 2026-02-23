8f1d7f8158e91f0ae25f6cf923ed838f9a1ec194
Arsen Korytskyi <114431254+imismim@users.noreply.github.com>
2026-02-23 21:38:26 +0200

    feat: add functionality to send email notifications for updated plans

--- src/checkouts/tasks.py
+++ src/checkouts/tasks.py
@@ -3,25 +3,21 @@ from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.conf import settings
 
+from reviewaggregator.constants import PLAN_EMAIL_SUBJECT, PLAN_EMAIL_MESSAGE
+
 User = get_user_model()
 
 
 @shared_task(bind=True, max_retries=3, default_retry_delay=60)
-def send_welcome_email(self, user_id, subject=None, message=None):
-    try:
-        user = User.objects.get(pk=user_id)
-    except User.DoesNotExist:
-        return
-    
-    username = user.username
+def send_greating_updated_plan(self, username, user_email, plan_name):
+
     try:
         send_mail(
-            subject=subject,
-            message=message(username),
+            subject=PLAN_EMAIL_SUBJECT,
+            message=PLAN_EMAIL_MESSAGE(username, plan_name),
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[user.email],
+            recipient_list=[user_email],
             fail_silently=False,
         )
     except Exception as exc:
         raise self.retry(exc=exc)
-
--- src/reviewaggregator/constants.py
+++ src/reviewaggregator/constants.py
@@ -2,3 +2,9 @@ WELCOME_EMAIL_SUBJECT = "Welcome to Review Aggregator!"
 WELCOME_EMAIL_MESSAGE = lambda username: f"""Hi {username},\n\nThanks for signing up for Review Aggregator. 
                                     \n\nYou can now start tracking and aggregating reviews for your SaaS products.
                                     \n\nGet started by logging in and exploring your dashboard.\n\n— The Review Aggregator Team"""
+                                    
+PLAN_EMAIL_SUBJECT = "Your Review Aggregator Plan was Updated"
+PLAN_EMAIL_MESSAGE = lambda username, plan_name: f"""Hi {username},\n\nYour Review Aggregator plan was updated to {plan_name}. 
+                                    \n\nYou can now enjoy the benefits of your new plan and continue tracking and aggregating reviews for your SaaS products.
+                                    \n\nIf you have any questions or need assistance, feel free to reach out to our support team.
+                                    \n\n— The Review Aggregator Team"""
