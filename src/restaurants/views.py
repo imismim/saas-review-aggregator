8a4b43e0aa5f41106f71fef37386a97d052667e0
Arsen Korytskyi <114431254+imismim@users.noreply.github.com>
2026-02-26 19:48:41 +0200

    feat: implement restaurant listing view and update navigation

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
 
 # Create your views here.
+
+
+class RestaurantListView(TemplateView):
+    template_name = 'restaurants/restaurants_list.html'
+    context_object_name = 'restaurants'
+    
+    
+
+restaurant_list = RestaurantListView.as_view()
