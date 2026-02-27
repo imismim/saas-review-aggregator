ea58a22c1dc6a18e6c4be9bb4944f676bc594def
Arsen Korytskyi <114431254+imismim@users.noreply.github.com>
2026-02-27 12:29:28 +0200

    feat: add subscription and restaurant limit mixins to enforce subscription checks and limits

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,47 +1,74 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 from .models import Restaurant
 from .form import RestaurantForm
+from .mixions import SubscriptionRequiredMixin, RestaurantLimitMixin
 
-
-class RestaurantListView(LoginRequiredMixin, ListView):
+class RestaurantListView(LoginRequiredMixin,
+                         ListView):
     model = Restaurant
     template_name = 'restaurants/restaurants_list.html'
     context_object_name = 'restaurants'
 
 
-class RestaurantDetailView(LoginRequiredMixin, DetailView):
+class RestaurantDetailView(LoginRequiredMixin,
+                           SubscriptionRequiredMixin,
+                           DetailView):
     model = Restaurant
     template_name = 'restaurants/restaurant_detail.html'
+    
+    messages_text_no_sub = "You need an active subscription to view restaurant details."
+    messages_text_no_user_sub = "You need to subscribe to view restaurant details."
+    messages_text_inactive_sub = "Your subscription is not active. Please subscribe to view restaurant details."
 
 
-class RestaurantCreateView(LoginRequiredMixin, CreateView):
+class RestaurantCreateView(LoginRequiredMixin,
+                           SubscriptionRequiredMixin,
+                           RestaurantLimitMixin,
+                           CreateView):
     model = Restaurant
     form_class = RestaurantForm
     template_name = 'restaurants/restaurant_add.html'
     success_url = reverse_lazy('restaurants-list')
-
+    
+    messages_text_no_sub = "You need an active subscription to add a restaurant."
+    messages_text_no_user_sub = "You need to subscribe to add a restaurant."
+    messages_text_inactive_sub = "Your subscription is not active. Please subscribe to add a restaurant."
+    messages_text_max_count = "You have reached the maximum number of restaurants allowed by your subscription. Please upgrade your subscription to add more restaurants."
+    
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
 
-class RestaurantUpdateView(LoginRequiredMixin, UpdateView):
+class RestaurantUpdateView(LoginRequiredMixin,
+                           SubscriptionRequiredMixin,
+                           UpdateView):
     model = Restaurant
     form_class = RestaurantForm
     template_name = 'restaurants/restaurant_edit.html'
-
+    
+    messages_text_no_sub = "You need an active subscription to edit a restaurant."
+    messages_text_no_user_sub = "You need to subscribe to edit a restaurant."
+    messages_text_inactive_sub = "Your subscription is not active. Please subscribe to edit a restaurant."
+    
     def get_success_url(self):
         return reverse_lazy('restaurant-detail', kwargs={'slug': self.object.slug})
 
 
-class RestaurantDeleteView(LoginRequiredMixin, DeleteView):
+class RestaurantDeleteView(LoginRequiredMixin,
+                           SubscriptionRequiredMixin,
+                           DeleteView):
     model = Restaurant
     template_name = 'restaurants/restaurant_confirm_delete.html'
     success_url = reverse_lazy('restaurants-list')
+    
+    messages_text_no_sub = "You need an active subscription to delete a restaurant."
+    messages_text_no_user_sub = "You need to subscribe to delete a restaurant."
+    messages_text_inactive_sub = "Your subscription is not active. Please subscribe to delete a restaurant."
+    
 
 
 restaurant_list = RestaurantListView.as_view()
