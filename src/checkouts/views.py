83ef0f5c7d31b5b423bc08f15aa070cfef3861bc
Arsen Korytskyi <114431254+imismim@users.noreply.github.com>
2026-02-27 23:18:29 +0200

    feat: refactor checkout flow and update active restaurant selection logic

--- src/checkouts/views.py
+++ src/checkouts/views.py
@@ -25,38 +25,6 @@ User = get_user_model()
 logger = logging.getLogger(__name__)
 
 @login_required
-def product_price_redirect_view(request, price_id=None, *args, **kwargs):
-    request.session['checkout_subscription_price_id'] = price_id
-    return redirect('stripe-checkout-start')
-
-
-@login_required
-def checkout_redirect_view(request):
-    subscription_price_id = request.session.get(
-        'checkout_subscription_price_id')
-    try:
-        obj = SubscriptionPrice.objects.get(id=subscription_price_id)
-    except SubscriptionPrice.DoesNotExist as e:
-        logger.error(f"SubscriptionPrice not found for id: {subscription_price_id}")
-        return redirect('pricing')
-
-    customer_stripe_id = request.user.customer.stripe_id
-    successful_url = request.build_absolute_uri(reverse('stripe-checkout-end'))
-    cancel_url = request.build_absolute_uri(reverse('pricing'))
-    selected_restaurant_ids = request.session.pop('selected_restaurant_ids', None)
-    print(f"selected_restaurant_ids from session: {selected_restaurant_ids}")
-    selected_restaurant_ids = ','.join(selected_restaurant_ids) if selected_restaurant_ids else ''
-    url = start_checkout_session(
-        customer_id=customer_stripe_id,
-        successful_url=successful_url,
-        cancel_url=cancel_url,
-        price_stripe_id=obj.stripe_id,
-        sub_metadata={"selected_restaurant_ids": selected_restaurant_ids},
-        raw=False
-    )
-    return redirect(url)
-
-@login_required
 def checkout_finalize_view(request):
     session_id = request.GET.get("session_id")
     
@@ -84,6 +52,59 @@ def checkout_finalize_view(request):
     
     return render(request, 'checkouts/success.html', context=context)
 
+
+
+class StartCheckoutView(LoginRequiredMixin, TemplateView):
+    template_name = 'checkouts/select_active_restaurant.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        price_id = self.kwargs.get('price_id')
+        sub_price_obj = get_object_or_404(SubscriptionPrice, id=price_id)
+        sub_obj = sub_price_obj.subscription
+        
+        context['max_count'] = sub_obj.max_count_restaurant
+        context['restaurants'] = Restaurant.objects.filter(user=self.request.user)
+        context['price_id'] = price_id
+        
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        
+        if context['max_count'] >= context['restaurants'].count():
+            return self.post(request, *args, **kwargs)
+        
+        return render(request, self.template_name, context=context)
+    
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        price_id = context['price_id']
+        max_count = context['max_count']
+        restaurant_ids_lst = request.POST.getlist('restaurant_ids')
+            
+        if max_count < len(restaurant_ids_lst):
+            messages.warning(request, f"You can only select up to {max_count} restaurants. You selected {len(restaurant_ids_lst)}.")
+            return redirect('sub-price-checkout', price_id=price_id)
+                
+        sub_price_obj = get_object_or_404(SubscriptionPrice, id=price_id)
+        customer_stripe_id = request.user.customer.stripe_id
+        successful_url = request.build_absolute_uri(reverse('stripe-checkout-end'))
+        cancel_url = request.build_absolute_uri(reverse('pricing'))
+        restaurant_ids = ','.join(restaurant_ids_lst)
+        
+        url = start_checkout_session(
+            customer_id=customer_stripe_id,
+            successful_url=successful_url,
+            cancel_url=cancel_url,
+            price_stripe_id=sub_price_obj.stripe_id,
+            sub_metadata={"selected_restaurant_ids": restaurant_ids},
+            raw=False
+        )
+        
+        return redirect(url)
+    
+start_checkout_view = StartCheckoutView.as_view()
 
 @csrf_exempt
 @require_POST
@@ -123,44 +144,3 @@ def stripe_webhook_view(request):
         logger.debug(f"Unhandled event: {event_type}")
 
     return HttpResponse(status=200)
-
-class SelectActiveRestaurantView(LoginRequiredMixin, TemplateView):
-    template_name = 'checkouts/select_active_restaurant.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        price_id = self.kwargs.get('price_id')
-        sub_price_obj = get_object_or_404(SubscriptionPrice, id=price_id)
-        sub_obj = sub_price_obj.subscription
-        
-        context['max_count'] = sub_obj.max_count_restaurant
-        context['restaurants'] = Restaurant.objects.filter(user=self.request.user)
-        context['price_id'] = price_id
-        
-        return context
-    
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        
-        if context['max_count'] >= context['restaurants'].count():
-            return redirect('sub-price-checkout', price_id=context['price_id'])
-        
-        return render(request, self.template_name, context=context)
-    def post(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        price_id = context['price_id']
-        max_count = context['max_count']
-        restaurant_ids = request.POST.getlist('restaurant_ids')
-        
-        if max_count < len(restaurant_ids):
-            messages.warning(request, f"You can only select up to {max_count} restaurants. You selected {len(restaurant_ids)}.")
-            return redirect('select-active-restaurant', price_id=price_id)
-            
-        request.session['selected_restaurant_ids'] = restaurant_ids
-        
-        sub = get_object_or_404(SubscriptionPrice, id=price_id)
-        
-        return redirect(sub.get_checkout_url())
-
-
-select_active_restaurant_view = SelectActiveRestaurantView.as_view()
