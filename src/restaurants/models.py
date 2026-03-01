705f412a034ce2b52c3601e23e922e8a80000dc3
Arsen Korytskyi <114431254+imismim@users.noreply.github.com>
2026-03-01 15:40:28 +0200

    fix: update restaurant model fields to allow null values and add unique constraint

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -16,7 +16,7 @@ class Restaurant(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='restaurants')
     
     place_id = models.CharField(max_length=100, null=True, blank=True) 
-    name = models.CharField(max_length=100, unique=True) 
+    name = models.CharField(max_length=100) 
     address = models.CharField(max_length=200, null=True, blank=True) # formatted_address
     phone_number = models.CharField(max_length=20, null=True, blank=True) # international_phone_number
     business_status = models.CharField(max_length=50, null=True, blank=True) 
@@ -24,18 +24,18 @@ class Restaurant(models.Model):
     website = models.URLField(null=True, blank=True) 
     google_maps_url = models.URLField(null=True, blank=True) # url
 
-    delivery = models.BooleanField(default=False) 
-    dine_in = models.BooleanField(default=False) 
-    serves_beer = models.BooleanField(default=False) 
-    serves_wine = models.BooleanField(default=False)
-    serves_breakfast = models.BooleanField(default=False)
-    serves_dinner = models.BooleanField(default=False)
-    serves_lunch = models.BooleanField(default=False)
-    serves_vegetarian_food = models.BooleanField(default=False)
-    takeout = models.BooleanField(default=False)
+    delivery = models.BooleanField(null=True, blank=True) 
+    dine_in = models.BooleanField(null=True, blank=True) 
+    serves_beer = models.BooleanField(null=True, blank=True) 
+    serves_wine = models.BooleanField(null=True, blank=True)
+    serves_breakfast = models.BooleanField(null=True, blank=True)
+    serves_dinner = models.BooleanField(null=True, blank=True)
+    serves_lunch = models.BooleanField(null=True, blank=True)
+    serves_vegetarian_food = models.BooleanField(null=True, blank=True)
+    takeout = models.BooleanField(null=True, blank=True)
     types = models.CharField(max_length=200, null=True, blank=True) 
-    wheelchair_accessible_entrance = models.BooleanField(default=False)
-    reservable = models.BooleanField(default=False)
+    wheelchair_accessible_entrance = models.BooleanField(null=True, blank=True)
+    reservable = models.BooleanField(null=True, blank=True)
     
     rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
     price_level = models.IntegerField(null=True, blank=True)
@@ -54,6 +54,7 @@ class Restaurant(models.Model):
         super().save(*args, **kwargs)
         
     class Meta:
+        unique_together = ('user', 'place_id')
         ordering = ['active', '-created_at']
         
 class Review(models.Model):
